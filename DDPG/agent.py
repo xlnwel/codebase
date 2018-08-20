df71a545607fb3b7665e9eccdb91027e0fa8b43d
xlnwel <122134545@qq.com>
2018-08-21 07:21:07 +0800

    Re-implement to deal with a list of experiences each time

--- DDPG/agent.py
+++ DDPG/agent.py
@@ -8,15 +8,17 @@ import random
 
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
+""" This implementation assume each time we receive a list of experiences """
 class ReplayBuffer():
     def __init__(self, sample_size, max_len=int(1e5)):
         self.buffer = deque(maxlen=max_len)
         self.sample_size = sample_size
         self.experience = namedtuple('experience', ('state', 'action', 'reward', 'next_state', 'done'))
 
-    def add(self, state, action, reward, next_state, done):
-        exp = self.experience(state, action, reward, next_state, done)
-        self.buffer.append(exp)
+    def add(self, states, actions, rewards, next_states, dones):
+        exps = [self.experience(s, a, r, ns, d)
+                for s, a, r, ns, d in zip(states, actions, rewards, next_states, dones)]
+        self.buffer.extend(exps)
 
     def sample(self):
         exps = random.sample(self.buffer, self.sample_size)
@@ -34,18 +36,21 @@ class ReplayBuffer():
 
 
 class Agent():
-    def __init__(self, state_size, action_size, batch_size=128, actor_alpha=1e-4, critic_alpha=1e-3, gamma=0.99, tau=1e-3):
+    def __init__(self, state_size, action_size, batch_size=128, 
+                 actor_alpha=1e-3, critic_alpha=1e-3, gamma=0.99, tau=1e-3, 
+                 device=device, actor_file=None, critic_file=None):
         # hyperparameters
         self.gamma = gamma
         self.tau = tau
+        self.noise_decay = 1 + 5e-6
         # env info
         self.state_size = state_size
         self.action_size = action_size
         # replay buffer
         self.buffer = ReplayBuffer(sample_size=batch_size)
         # actor-critic
-        self.actor_main = Actor(state_size, action_size).to(device)
-        self.critic_main = Critic(state_size, action_size).to(device)
+        self.actor_main = Actor(state_size, action_size, param_file=actor_file).to(device)
+        self.critic_main = Critic(state_size, action_size, param_file=critic_file).to(device)
         # target actor-critic
         self.actor_target = copy.deepcopy(self.actor_main)
         self.critic_target = copy.deepcopy(self.critic_main)
@@ -53,30 +58,28 @@ class Agent():
         self.actor_optimizer = torch.optim.Adam(self.actor_main.parameters(), lr=actor_alpha)
         self.critic_optimizer = torch.optim.Adam(self.critic_main.parameters(), lr=critic_alpha)
 
-        self.noise_decay = 1 + 1e-4
-
-    def act(self, state):
-        state = torch.from_numpy(state).float().to(device)
+    def act(self, states):
+        states = torch.from_numpy(states).float().to(device)
         # add noise to parameters
         saved_params = []
         for param in self.actor_main.parameters():
             saved_params.append(copy.deepcopy(param))
             param = param + torch.normal(mean=0.0, std=torch.ones_like(param) / (10 * self.noise_decay))
 
-        self.noise_decay *= 1 + 1e-4
+        self.noise_decay *= 1 + 5e-6
         
         self.actor_main.eval()
         with torch.no_grad():
-           action = self.actor_main(state).cpu().numpy()
+           actions = self.actor_main(states).cpu().numpy()
         self.actor_main.train()
         # restore parameters
         for param, saved_param in zip(self.actor_main.parameters(), saved_params):
             param = saved_param
 
-        return np.clip(action, -1, 1)
+        return np.clip(actions, -1, 1)
 
-    def step(self, state, action, reward, next_state, done):
-        self.buffer.add(state, action, reward, next_state, done)
+    def step(self, states, actions, rewards, next_states, dones):
+        self.buffer.add(states, actions, rewards, next_states, dones)
 
         if len(self.buffer) > self.buffer.sample_size + 100:
             self._learn()
