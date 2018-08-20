8a121b91c6d4d364f1db52c19fcf6d3d66150ec3
xlnwel <122134545@qq.com>
2018-08-21 07:19:50 +0800

    Add parameter loader

--- DDPG/model.py
+++ DDPG/model.py
@@ -3,12 +3,15 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class Actor(nn.Module):
-    def __init__(self, state_size, action_size):
+    def __init__(self, state_size, action_size, param_file=None):
         super().__init__()
         self.fc1 = nn.Linear(state_size, 512)
         self.fc2 = nn.Linear(512, 256)
         self.fc3 = nn.Linear(256, action_size)
-        self._reset_params()
+        if param_file:
+            self.load_params(param_file)
+        else:
+            self._reset_params()
 
     def _reset_params(self):
         nn.init.kaiming_normal_(self.fc1.weight)
@@ -18,20 +21,28 @@ class Actor(nn.Module):
         nn.init.xavier_normal_(self.fc3.weight)
         nn.init.constant_(self.fc3.bias, 0)
 
+    def load_params(self, param_file):
+        self.load_state_dict(torch.load(param_file))
+        
     def forward(self, state):
         x = state
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
-        return 2 * torch.tanh(self.fc3(x))
+        return torch.tanh(self.fc3(x))
 
 
 class Critic(nn.Module):
-    def __init__(self, state_size, action_size):
+    def __init__(self, state_size, action_size, param_file=None):
         super().__init__()
         self.fc1 = nn.Linear(state_size, 512)
+        self.bn1 = nn.BatchNorm1d(512)
         self.fc2 = nn.Linear(512 + action_size, 256)
+        self.bn2 = nn.BatchNorm1d(256)
         self.fc3 = nn.Linear(256, 1)
-        self._reset_params()
+        if param_file:
+            self.load_params(param_file)
+        else:
+            self._reset_params()
 
     def _reset_params(self):
         nn.init.kaiming_normal_(self.fc1.weight)
@@ -41,9 +52,11 @@ class Critic(nn.Module):
         nn.init.xavier_normal_(self.fc3.weight)
         nn.init.constant_(self.fc3.bias, 0)
     
+    def load_params(self, param_file):
+        self.load_state_dict(torch.load(param_file))
+
     def forward(self, state, action):
-        x = state
-        x = F.relu(self.fc1(x))
+        x = F.relu(self.bn1(self.fc1(state)))
         x = torch.cat((x, action), dim=1)
-        x = F.relu(self.fc2(x))
+        x = F.relu(self.bn2(self.fc2(x)))
         return self.fc3(x)
